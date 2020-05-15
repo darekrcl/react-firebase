6050054f76e8117e6e46ccf2cd13ec9c2bd70ef9
darekrcl <darek.rcl@gmail.com>
2020-05-15 18:29:14 +0200

    kosmetyka

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import List from './components/List'
+import AddTodo from './components/AddTodo';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Harmonogram</h1>
+      <List />
+      <AddTodo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import firebase from '../firebase'
+
+const List = () => {
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        const unsubscribe = firebase
+            .firestore()
+            .collection('todos')
+            .onSnapshot(snapshot => {
+                const newTodos = snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }))
+                setTodos(newTodos)
+            })
+        return () => unsubscribe()
+    }, [])
+
+    const reverseDate = str => {
+        return str.split("-").reverse().join("-");
+    }
+
+    return (
+        <>
+            <ol>
+                {todos.map(todo =>
+                    <li key={todo.id}><span>{todo.taskGroup}</span> <span>{todo.taskName}</span><span> {reverseDate(todo.taskDate)}</span> <span>{todo.taskTime}</span></li>
+                )}
+            </ol>
+        </>
+    )
+}
+
+export default List;
